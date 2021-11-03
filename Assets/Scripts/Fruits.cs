16e95d52a36a46946c1604b788951264d12d5b1f
Toukir islam <gameshovo@gmail.com>
2021-11-03 16:05:12 +0100

    fruit ninja firts commit with only adding fruit scripts

---
+++ Assets/Scripts/Fruits.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruits : MonoBehaviour
+{
+    public GameObject slicedFruitPrefeb;
+    public float explotinRadius = 5f;
+    public void CreateSlicedFruit()
+    {
+        GameObject inst = Instantiate(slicedFruitPrefeb, transform.position,transform.rotation);
+
+        Rigidbody[] rbsliced = inst.transform.GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody rigidbody in rbsliced) //the collection that we go through , type Rigidbody in rbsliced name object rigidbody 
+        {
+            rigidbody.transform.rotation = Random.rotation;
+            rigidbody.AddExplosionForce(Random.Range(500, 1000), transform.position, explotinRadius);
+        }
+
+        Destroy(gameObject);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+            CreateSlicedFruit();
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject fruit;
+    public Transform[] spawnPlaces;
+    public float minWait = 0.3f;
+    public float maxWait = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(SpwanFruits());
+    }
+    IEnumerator SpwanFruits()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWait,maxWait));
+            GameObject fruits =  Instantiate(fruit, transform.position, transform.rotation);
+        }
+    }
+}
